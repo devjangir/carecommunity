efa4295af8997b7870f81cde0bd0f097be92c477
devdutt jangir <djangir@mphrx.com>
2016-06-18 11:45:46 +0300

    project created

---
+++ CareCommunity/CareCommunity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CareCommunity
+//
+//  Created by Devdutt on 18/06/16.
+//  Copyright © 2016 devjangir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CareCommunity/CareCommunity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CareCommunity
+//
+//  Created by Devdutt on 18/06/16.
+//  Copyright © 2016 devjangir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
